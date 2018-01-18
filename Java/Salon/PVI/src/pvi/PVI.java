276abd845fe142fe0322fb5e78c3259c4d3e6551
joshuansu <joshuansu0897@gmail.com>
2018-01-17 18:15:08 -0700

    subiendo lo basico de Java, primer dia de clases

---
+++ Java/Salon/PVI/src/pvi/PVI.java
@@ -0,0 +1,16 @@
+package pvi;
+
+/**
+ *
+ * @author joshuansu
+ */
+public class PVI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ Java/Salon/PVI/src/pvi/Persona.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pvi;
+
+/**
+ *
+ * @author joshuansu
+ */
+public class Persona {
+
+    private char sexo; // F femenino | M masculino
+    private String nombre;
+    private double alutra;
+    private int edad;
+    private boolean lentes;
+
+    public Persona(char sexo, String nombre, double alutra, int edad, boolean lentes) {
+        this.sexo = sexo;
+        this.nombre = nombre;
+        this.alutra = alutra;
+        this.edad = edad;
+        this.lentes = lentes;
+    }
+
+    public boolean isLentes() {
+        return lentes;
+    }
+
+    public void setLentes(boolean lentes) {
+        this.lentes = lentes;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getAlutra() {
+        return alutra;
+    }
+
+    public void setAlutra(double alutra) {
+        this.alutra = alutra;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+}
