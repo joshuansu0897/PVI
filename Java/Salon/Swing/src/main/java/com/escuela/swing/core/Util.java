c33842798dc3b5740841c67befa96bb4c7713e99
joshuansu <joshuansu0897@gmail.com>
2018-01-29 19:00:56 -0700

    se inicio un bloc de notas

---
+++ Java/Salon/Swing/src/main/java/com/escuela/swing/core/Util.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.escuela.swing.core;
+
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/**
+ *
+ * @author joshuansu
+ */
+public class Util {
+    public static String fileChooser(String dir) {
+        return fileChooser(dir, null);
+    }
+
+    public static String fileChooser(String dir, String extension) {
+        if (dir != null && !dir.trim().equals("")) {
+            dir = dir.trim();
+        } else {
+            dir = System.getProperty("user.home");
+        }
+        String path;
+        JFileChooser fc = new JFileChooser();
+        if (extension != null && !extension.trim().equals("")) {
+            FileNameExtensionFilter ex = new FileNameExtensionFilter(extension.toUpperCase(), extension.toLowerCase());
+            fc.setFileFilter(ex);
+        }
+        fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fc.setCurrentDirectory(new File(dir));
+        int res = fc.showOpenDialog(null);
+        if (res == JFileChooser.APPROVE_OPTION) {
+            path = fc.getSelectedFile().getAbsolutePath();
+        } else {
+            path = null;
+        }
+        return path;
+    }
+}
