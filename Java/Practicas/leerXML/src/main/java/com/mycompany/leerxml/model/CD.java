585a821fbc5faef96d4b21ca60410b40cde470f4
Joshua Saucedo <joshuansu0897@gmail.com>
2018-04-02 18:55:52 -0700

    nueva practica

---
+++ Java/Practicas/leerXML/src/main/java/com/mycompany/leerxml/model/CD.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.leerxml.model;
+
+/**
+ *
+ * @author joshuansu
+ */
+public class CD {
+
+    private String title;
+    private String artist;
+    private String country;
+    private String company;
+    private float price;
+    private int year;
+
+    public CD() {
+    }
+
+    public CD(String title, String artist, String country, String company, float price, int year) {
+        this.title = title;
+        this.artist = artist;
+        this.country = country;
+        this.company = company;
+        this.price = price;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "CD{" + "title=" + title + ", artist=" + artist + ", country=" + country + ", company=" + company + ", price=" + price + ", year=" + year + '}';
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
